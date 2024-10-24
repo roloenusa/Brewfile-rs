b2a97b04ce134f3f2072c590e765e6526bb9f1ba
Juan Delgado <juan.delgado@fortisgames.com>
2024-10-24 15:36:54 -0700

    Chapter 3: Alternatives

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::error::Error;
 
 use nom::bytes::complete::tag;
+use nom::branch::alt;
 use nom::IResult;
 
 fn parse_input(input: &str) -> IResult<&str, &str> {
     tag("abc")(input)
+}
+
+fn parse_abc_or_def(input: &str) -> IResult<&str, &str> {
+    alt((
+        tag("abc"),
+        tag("def")
+    ))(input)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -13,6 +21,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     assert_eq!(output, "abc");
 
     assert!(parse_input("defWorld").is_err());
+
+
+    // Alt combinator
+    let (leftover_input, output) = parse_abc_or_def("abcWorld")?;
+    assert_eq!(leftover_input, "World");
+    assert_eq!(output, "abc");
+
+    let (leftover_input, output) = parse_abc_or_def("defWorld")?;
+    assert_eq!(leftover_input, "World");
+    assert_eq!(output, "def");
+
+    assert!(parse_abc_or_def("ghiWorld").is_err());
+
     Ok(())
 }
 
