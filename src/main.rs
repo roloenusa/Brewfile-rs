0635eb529a615acc06212a3f2bd77ed21b5bee0f
Juan Delgado <juan.delgado@fortisgames.com>
2024-10-24 15:31:46 -0700

    Chapter 2: Tags and Characters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello world");
+use std::error::Error;
+
+use nom::bytes::complete::tag;
+use nom::IResult;
+
+fn parse_input(input: &str) -> IResult<&str, &str> {
+    tag("abc")(input)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (leftover_input, output) = parse_input("abcWorld")?;
+    assert_eq!(leftover_input, "World");
+    assert_eq!(output, "abc");
+
+    assert!(parse_input("defWorld").is_err());
+    Ok(())
 }
 
