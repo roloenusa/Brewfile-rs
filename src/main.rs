e58dd4b4fcbd3027e3e6d716310d0220864e351d
roloenusa <roloenusa@msn.com>
2024-10-25 00:07:58 -0700

    Chapter 5: Parsers which use a predicate

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,20 @@
 use std::error::Error;
 
-use nom::bytes::complete::tag;
-use nom::sequence::{delimited, separated_pair};
+use nom::bytes::complete::{take_until, take_while};
+use nom::sequence::terminated;
 use nom::IResult;
 
-// This is the type we will parse into
-#[derive(Debug, PartialEq)]
-pub struct Coordinate {
-    pub x: i32,
-    pub y: i32,
-}
-
-// 1. Nom has an in-built i32 parser.
-use nom::character::complete::{i32, space0};
-
-// 2. Use the `separated_pair` parser to combine two parsers (in this case,
-//    both `i32`), ignoring something in-between.
-fn parse_integer_pair(input: &str) -> IResult<&str, (i32, i32)> {
-    separated_pair(
-        i32,
-        // tag(", "),
-        parse_delimiter,
-        i32
-    )(input)
-}
-
-fn parse_delimiter(input: &str) -> IResult<&str, &str> {
-    let (remaining, _blank) = space0(input)?;
-    let (remaining, _comma) = tag(",")(remaining)?;
-    space0(remaining)
-}
-
-// 3. Use the `delimited` parser to apply a parser, ignoring the results
-//    of two surrounding parsers.
-fn parse_coordinate(input: &str) -> IResult<&str, Coordinate> {
-    let (remaining, (x, y)) = delimited(
-        tag("("),
-        parse_integer_pair,
-        tag(")")
-    )(input)?;
-
-    // Note: We could construct this by implementing `From` on `Coordinate`,
-    // We don't, just so it's obvious what's happening.
-    Ok((remaining, Coordinate {x, y}))
+fn parse_sentence(input: &str) -> IResult<&str, &str> {
+    terminated(take_until("."), take_while(|c| c == '.' || c == ' '))(input)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (_, parsed) = parse_coordinate("(3, 5)")?;
-    assert_eq!(parsed, Coordinate {x: 3, y: 5});
+    let (remaining, parsed) = parse_sentence("I am Tom. I write Rust")?;
+    assert_eq!(parsed, "I am Tom");
+    assert_eq!(remaining, "I write Rust");
 
-    let (_, parsed) = parse_coordinate("(2, -4)")?;
-    assert_eq!(parsed, Coordinate {x: 2, y: -4});
-
-    let (_, parsed) = parse_coordinate("(2    ,     -4)")?;
-    assert_eq!(parsed, Coordinate {x: 2, y: -4});
-
-    let parsing_error = parse_coordinate("(3,)");
-    assert!(parsing_error.is_err());
-
-    let parsing_error = parse_coordinate("(,3)");
-    assert!(parsing_error.is_err());
-
-    let parsing_error = parse_coordinate("Ferris");
-    assert!(parsing_error.is_err());
+    let parser_error = parse_sentence("Not a sentence (no period at the end)");
+    assert!(parser_error.is_err());
 
     Ok(())
 }
