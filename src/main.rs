46be24e95bd52d760a01c0fa0dbfd4949792e84f
roloenusa <roloenusa@msn.com>
2024-10-26 00:17:58 -0700

    Add nom and sample parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use nom::{bytes::complete::tag, character::complete::multispace0, sequence::delimited, IResult};
+
+fn parse(input: &str) -> IResult<&str, &str> {
+    let (remainder, result) = delimited(
+        multispace0,
+        tag("brew"),
+        multispace0
+    )(input)?;
+
+    Ok((remainder, result))
+}
+
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+
     println!("{}", src);
+
+    let (remainder, result) = parse(&src).unwrap();
+    println!("Result: {result} - {remainder}");
 }
 
