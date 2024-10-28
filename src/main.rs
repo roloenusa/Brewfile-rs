9e69e411fc72431ea603390f9baafcd4474bf964
roloenusa <roloenusa@msn.com>
2024-10-27 23:45:26 -0700

    Successfully parsing strings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 use nom::branch::alt;
-use nom::bytes::complete::{escaped, is_not, take_while_m_n};
+use nom::bytes::complete::{escaped, escaped_transform, is_not, take_while_m_n};
 use nom::character::complete::{alphanumeric1, line_ending, multispace0, one_of, space0};
 use nom::character::streaming::{char, multispace1};
 use nom::bytes::complete::tag;
@@ -14,6 +14,7 @@ use nom::{IResult, Parser};
 enum Command {
     Brew(String),
 }
+
 /// A nom parser has the following signature:
 /// `Input -> IResult<Input, Output, Error>`, with `IResult` defined as:
 /// `type IResult<I, O, E = (I, ErrorKind)> = Result<(I, O), Err<E>>;`
@@ -30,8 +31,8 @@ enum Command {
 /// of the input data. and there is no allocation needed. This is the main idea
 /// behind nom's performance.
 fn parse_str<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
-    println!("------ inside parse_str {}", i);
-  escaped(alphanumeric1, '\\', one_of("\"n\\"))(i)
+    // escaped(alt((alphanumeric1, space0)), '\\', one_of("\"n\\"))(i)
+    escaped(is_not("\\\""), '\\', one_of("\"nt\\"))(i)
 }
 
 /// this parser combines the previous `parse_str` parser, that recognizes the
@@ -48,7 +49,6 @@ fn parse_str<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str
 fn string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
   input: &'a str,
 ) -> IResult<&'a str, &'a str, E> {
-    println!("------ inside string {:#?}", input);
   context(
     "string",
     preceded(char('\"'), cut(terminated(parse_str, char('\"')))),
@@ -56,183 +56,22 @@ fn string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
   .parse(input)
 }
 
-// parser combinators are constructed from the bottom up:
-// first we write parsers for the smallest elements (escaped characters),
-// then combine them into larger parsers.
-
-/// Parse a unicode sequence, of the form u{XXXX}, where XXXX is 1 to 6
-/// hexadecimal numerals. We will combine this later with parse_escaped_char
-/// to parse sequences like \u{00AC}.
-fn parse_unicode<'a, E>(input: &'a str) -> IResult<&'a str, char, E>
-where
-  E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
-{
-  // `take_while_m_n` parses between `m` and `n` bytes (inclusive) that match
-  // a predicate. `parse_hex` here parses between 1 and 6 hexadecimal numerals.
-  let parse_hex = take_while_m_n(1, 6, |c: char| c.is_ascii_hexdigit());
-
-  // `preceded` takes a prefix parser, and if it succeeds, returns the result
-  // of the body parser. In this case, it parses u{XXXX}.
-  let parse_delimited_hex = preceded(
-    char('u'),
-    // `delimited` is like `preceded`, but it parses both a prefix and a suffix.
-    // It returns the result of the middle parser. In this case, it parses
-    // {XXXX}, where XXXX is 1 to 6 hex numerals, and returns XXXX
-    delimited(char('{'), parse_hex, char('}')),
-  );
-
-  // `map_res` takes the result of a parser and applies a function that returns
-  // a Result. In this case we take the hex bytes from parse_hex and attempt to
-  // convert them to a u32.
-  let parse_u32 = map_res(parse_delimited_hex, move |hex| u32::from_str_radix(hex, 16));
-
-  // map_opt is like map_res, but it takes an Option instead of a Result. If
-  // the function returns None, map_opt returns an error. In this case, because
-  // not all u32 values are valid unicode code points, we have to fallibly
-  // convert to char with from_u32.
-  map_opt(parse_u32, |value| std::char::from_u32(value))(input)
-}
-
-/// Parse an escaped character: \n, \t, \r, \u{00AC}, etc.
-fn parse_escaped_char<'a, E>(input: &'a str) -> IResult<&'a str, char, E>
-where
-  E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
-{
-  preceded(
-    char('\\'),
-    // `alt` tries each parser in sequence, returning the result of
-    // the first successful match
-    alt((
-      parse_unicode,
-      // The `value` parser returns a fixed value (the first argument) if its
-      // parser (the second argument) succeeds. In these cases, it looks for
-      // the marker characters (n, r, t, etc) and returns the matching
-      // character (\n, \r, \t, etc).
-      value('\n', char('n')),
-      value('\r', char('r')),
-      value('\t', char('t')),
-      value('\u{08}', char('b')),
-      value('\u{0C}', char('f')),
-      value('\\', char('\\')),
-      value('/', char('/')),
-      value('"', char('"')),
-    )),
-  )(input)
-}
-
-/// Parse a backslash, followed by any amount of whitespace. This is used later
-/// to discard any escaped whitespace.
-fn parse_escaped_whitespace<'a, E: ParseError<&'a str>>(
-  input: &'a str,
-) -> IResult<&'a str, &'a str, E> {
-  preceded(char('\\'), multispace1)(input)
-}
-
-/// Parse a non-empty block of text that doesn't include \ or "
-fn parse_literal<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, &'a str, E> {
-  // `is_not` parses a string of 0 or more characters that aren't one of the
-  // given characters.
-  let not_quote_slash = is_not("\"\\");
-
-  // `verify` runs a parser, then runs a verification function on the output of
-  // the parser. The verification function accepts out output only if it
-  // returns true. In this case, we want to ensure that the output of is_not
-  // is non-empty.
-  verify(not_quote_slash, |s: &str| !s.is_empty())(input)
-}
-
-
-/// A string fragment contains a fragment of a string being parsed: either
-/// a non-empty Literal (a series of non-escaped characters), a single
-/// parsed escaped character, or a block of escaped whitespace.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum StringFragment<'a> {
-  Literal(&'a str),
-  EscapedChar(char),
-  EscapedWS,
-}
-
-
-/// Combine parse_literal, parse_escaped_whitespace, and parse_escaped_char
-/// into a StringFragment.
-fn parse_fragment<'a, E>(input: &'a str) -> IResult<&'a str, StringFragment<'a>, E>
-where
-  E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
-{
-  alt((
-    // The `map` combinator runs a parser, then applies a function to the output
-    // of that parser.
-    map(parse_literal, StringFragment::Literal),
-    map(parse_escaped_char, StringFragment::EscapedChar),
-    value(StringFragment::EscapedWS, parse_escaped_whitespace),
-  ))(input)
-}
-
-/// Parse a string. Use a loop of parse_fragment and push all of the fragments
-/// into an output string.
-fn parse_string<'a, E>(input: &'a str) -> IResult<&'a str, String, E>
-where
-  E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
-{
-  // fold_many0 is the equivalent of iterator::fold. It runs a parser in a loop,
-  // and for each output value, calls a folding function on each output value.
-  let build_string = fold_many0(
-    // Our parser function– parses a single string fragment
-    parse_fragment,
-    // Our init value, an empty string
-    String::new,
-    // Our folding function. For each fragment, append the fragment to the
-    // string.
-    |mut string, fragment| {
-      match fragment {
-        StringFragment::Literal(s) => string.push_str(s),
-        StringFragment::EscapedChar(c) => string.push(c),
-        StringFragment::EscapedWS => {}
-      }
-      string
-    },
-  );
-
-  // Finally, parse the string. Note that, if `build_string` could accept a raw
-  // " character, the closing delimiter " would never match. When using
-  // `delimited` with a looping parser (like fold_many0), be sure that the
-  // loop won't accidentally match your closing delimiter!
-  delimited(char('"'), build_string, char('"'))(input)
-}
-
-// fn parse_quoted_str(input: &str) -> IResult<&str, &str> {
-//
-//     delimited(
-//         tag(" \""),
-//         // tag("asdf"),
-//         rest,
-//         tag("\"")
-//     )(input)
-// }
-
 fn parse_command(input: &str) -> IResult<&str, Command>{
-    let (remainder, command) = tag("brew")(input)?;
-    match command {
-        "brew" => {
-            println!("===== remainder {:#?}", remainder);
-            // match parse_string::<()>(remainder) {
-            match preceded(space0, string::<()>)(remainder) {
-                Ok((remainder, target)) =>  {
-                    println!("===== remainder2 {:#?}", remainder);
-                    Ok((remainder, Command::Brew(target.to_string())))
-                },
-                _ => panic!("Error parsing"),
-            }
+    let (remainder, _brew_command) = tag("brew")(input)?;
+
+    match preceded(space0, string::<()>)(remainder) {
+        Ok((remainder, target)) =>  {
+            Ok((remainder, Command::Brew(target.to_string())))
         },
-        _ => panic!("Unknown command"),
+        Err(e) => panic!("Error parsing {:#?}", e),
     }
 }
 
 fn parse_line(input: &str) -> IResult<&str, Command> {
-    println!("--- parse line {:#?}", input);
-    let (remainder, result) = preceded(
+    let (remainder, result) = delimited(
         multispace0,
         parse_command,
+        multispace0,
     )(input)?;
 
     Ok((remainder, result))
@@ -247,8 +86,14 @@ fn main() {
 
     println!("{}", src);
 
-    let (remainder, result) = parse_input(&src).unwrap();
-    println!("Result: {:#?}", result);
-    println!("Remainder: {}", remainder);
+    let (_remainder, result) = parse_input(&src).unwrap();
+    println!("{:#?}", result);
+
+    // match &result[0] {
+    //     Command::Brew(text) => println!("---- text: {}", text),
+    //     _ => panic!("wtf"),
+    // }
+    // println!("Result: {}", result[0]);
+    // println!("Remainder: {}", remainder);
 }
 
