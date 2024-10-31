fcfd0fd094070657b1de9022b84a10e146068d3b
roloenusa <roloenusa@msn.com>
2024-10-30 23:28:18 -0700

    Fix taps and multiple arguments

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ use string_parser::string;
 #[derive(Debug)]
 enum Command<'a> {
     Brew(Brew<'a>),
+    Tap(Tap<'a>),
+}
+
+#[derive(Debug, Clone, Default)]
+struct Tap<'a> {
+    user_repo: &'a str,
+    url: Option<&'a str>,
 }
 
 #[derive(Debug, Clone)]
@@ -23,17 +30,57 @@ struct Brew<'a> {
     args: Vec<&'a str>,
 }
 
+
+fn is_last(input: &str) -> IResult<&str, bool> {
+    let (remainder, comma) = alt((parse_spacer, multispace0))(input)?;
+    match comma {
+        "," => Ok((remainder, false)),
+        _ => Ok((remainder, true)),
+    }
+}
+
+fn parse_spacer(input: &str) -> IResult<&str, &str> {
+    let (remainder, comma) = preceded(space0, terminated(tag(","), space0))(input)?;
+    Ok((remainder, comma))
+}
+
 fn parse_list(input: &str) -> IResult<&str, Vec<&str>> {
     delimited(
         terminated(tag("["), space0),
-        separated_list0(tuple((space0, tag(","), space0)), string),
+        separated_list0(parse_spacer, string),
         preceded(space0, tag("]")),
     )(input)
 }
 
+fn parse_tap(input: &str) -> IResult<&str, Tap> {
+    let (remainder, user_repo) = string::<()>(input).unwrap();
+    // let mut tap = Tap ser_repo);
+
+    let (remainder, result) = is_last(remainder)?;
+
+    if result {
+        return Ok((remainder, Tap { user_repo, url: None }))
+    }
+
+    let (remainder, url) = string::<()>(remainder).unwrap();
+
+    // Build the object we need to return
+    Ok((remainder, Tap { user_repo, url: Some(url) }))
+}
+
 fn parse_brew(input: &str) -> IResult<&str, Brew> {
     let (remainder, target) = string::<()>(input).unwrap();
-    let (remainder, _) = tuple((space0, tag(","), space0))(remainder)?;
+    let (remainder, result) = is_last(remainder)?;
+
+    if result {
+        // Build the object we need to return
+        let brew = Brew {
+            command: target,
+            args: Vec::new()
+        };
+
+        return Ok((remainder, brew))
+    }
 
     // Grab the parameter, ignore white spaces after.
     let (remainder, _param) = terminated(tag("args"), tag(":"))(remainder)?;
@@ -53,9 +100,16 @@ fn parse_brew(input: &str) -> IResult<&str, Brew> {
 
 fn parse_command(input: &str) -> IResult<&str, Command>{
     // Commands should always be followed by a space
-    let (remainder, _brew_command) = terminated(tag("brew"), space0)(input)?;
+    let (remainder, _brew_command) = terminated(
+        alt((tag("brew"), tag("tap"))),
+        space0)
+        .parse(input)?;
 
     match _brew_command {
+        "tap" => {
+            let (remainder, tap) = parse_tap(remainder)?;
+            Ok((remainder, Command::Tap(tap)))
+        }
         "brew" => {
             let (remainder, brew) = parse_brew(remainder)?;
             Ok((remainder, Command::Brew(brew)))
